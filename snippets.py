15cf28947c65917e92566563b2c1452dd803a386
Craig Silverstein <csilvers@khanacademy.org>
2014-05-23 15:26:02 -0700

    A few final fixes to get the code working under python2.7.
    1) I didn't finish turning this from a CGI script into a WSGI script.
    This involved getting rid of main() and moving the endpoint in
    app.yaml to the wsgi script instead.
    
    2) remote_api is specified differently, as well, in wsgi-land.
    
    3) Django defaults to 1.2 (or greater) in python27, so I don't need to
    -- indeed, cannot -- specify that anymore.
    
    Auditors: marcos
    
    Test Plan:
    Updated snippets.py to prod and visited
       http://weekly-snippets.appspot.com/
    and saw it render correctly.

--- snippets.py
+++ snippets.py
@@ -14,16 +14,10 @@ import os
 import time
 import urllib
 
-# Before importing anything from appengine, set the django version we want.
-# Taken from http://stackoverflow.com/questions/4994913/app-engine-default-django-version-change
-from google.appengine.dist import use_library
-use_library('django', '1.2')
-
 from google.appengine.api import mail
 from google.appengine.api import users
 from google.appengine.ext import webapp
 from google.appengine.ext.webapp import template
-from google.appengine.ext.webapp.util import run_wsgi_app
 from google.appengine.ext import db
 
 
@@ -635,11 +629,3 @@ application = webapp.WSGIApplication([('/', UserPage),
                                        hipchatlib.TestSendToHipchat),
                                       ],
                                       debug=True)
-
-
-def main():
-    run_wsgi_app(application)
-
-
-if __name__ == "__main__":
-    main()
