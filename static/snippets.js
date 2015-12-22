a0861b112a8d8ef96a7aee88b017448658e1f3f0
rileyjshaw <rileyjshaw@gmail.com>
2015-12-22 11:23:07 -0500

    Rewrite snippets.js
    Summary:
    As we've added more functionality to snippets.js, it's become a bit of a soup. After a non-obvious-but-should-have-been-obvious bug with event targets landed yesterday, it seemed time to restructure some of the copypastas in snippets.js. Hopefully this makes adding new features to the User Snippets page a bit less intimidating going forward :)
    
    Speaking of which, this adds a feature! As mentioned in D24254,
    
     > Known issues: toggling private / markdown state doesn't register a snippet as "dirty". This has never worked, as far as I can tell.
    
    Now it does! Checkbox state is recorded, so they now contribute to a snippet's "dirty" status. They also reset when "Clear Changes" is clicked.
    
    I still think that a React rewrite would be awesome, and if this file goes away //tomorrow// I'd be happy. But for now, hopefully it's a step in the right direction.
    
    Depends on D24254.
    
    Test Plan:
    - Clicked around and typed a bunch with the console open,
    - would appreciate a second pair of eyes on it if you've got a few minutes.
    
    Reviewers: michelle
    
    Reviewed By: michelle
    
    Subscribers: anju
    
    Differential Revision: https://phabricator.khanacademy.org/D24257

--- static/snippets.js
+++ static/snippets.js
@@ -1,103 +1,142 @@
+// Handlers for the user_snippets.html template.
+//
 $(function() {
+    // A User Snippet class constructor to be called on each
+    // `.user-snippet-form`. Handles a bunch of side-effects and adds event
+    // listeners to form children within the constructor.
+    function Snippet($parentForm) {
+        this.$el = $parentForm;
 
-    // save save the state of the snippet in .data and disable buttons
-    var saveLocalSnippet = function(textarea) {
-        var $parentForm = $(textarea).closest(".user-snippet-form");
-        $(textarea).data("orig", $(textarea).val());
-        $parentForm.removeClass("dirty").
-            find(".undo-button, .save-button").
-            prop("disabled", true);
-        $parentForm.find(".snippet-preview-container").hide();
+        // Child elements of the parent form.
+        // We use "secret" instead of "private" because "private" is reserved.
+        this.$markdownInput = $parentForm.find("input[name=is_markdown]");
+        this.$noneTag = $parentForm.find(".snippet-tag-none");
+        this.$preview = $parentForm.find(".snippet-preview-container");
+        this.$previewText = $parentForm.find(".snippet-preview");
+        this.$saveButton = $parentForm.find(".save-button");
+        this.$secretInput = $parentForm.find("input[name=private]");
+        this.$secretTag = $parentForm.find(".snippet-tag-private");
+        this.$textarea = $parentForm.find("textarea");
+        this.$undoButton = $parentForm.find(".undo-button");
+
+        // Child element collections.
+        this.$buttons = this.$saveButton.add(this.$undoButton);
+        this.$inputs =
+            this.$markdownInput.add(this.$secretInput).add(this.$textarea);
+
+        // Internal state of the Snippet.
+        // Matches the initial Snippet values to allow undo and dirty checks.
+        this.oldState = {};
+        // Holds the current Snippet values. We'll initialize it soon...
+        this.state = {
+            content: null,
+            markdown: null,
+            secret: null,
+        };
+
+        // Attach event listeners.
+        // Internal state should be kept up-to-date with our inputs.
+        this.$textarea.on("keyup change", (function() {
+            this.state.content = this.$textarea.val();
+        }).bind(this));
+        this.$markdownInput.on("change", (function() {
+            this.state.markdown = this.$markdownInput.prop("checked");
+        }).bind(this));
+        this.$secretInput.on("change", (function() {
+            this.state.secret = this.$secretInput.prop("checked");
+        }).bind(this));
+
+        // Pull our initial input values into internal state.
+        this.$inputs.trigger("change");
+
+        // Re-render whenever an input changes.
+        this.$inputs.on("keyup change", this.render.bind(this));
+
+        // Save and undo buttons, available when state is dirty.
+        this.$undoButton.on("click", this.undo.bind(this));
+        this.$saveButton.on("click", this.submit.bind(this));
+
+        // Finally, save (this also triggers a render).
+        this.save();
+    }
+
+    // Save the internal state of a Snippet to allow undos.
+    Snippet.prototype.save = function(e) {
+        e && e.preventDefault && e.preventDefault();
+        // Save the current values into .data.
+        this.oldState.content = this.state.content;
+        this.oldState.markdown = this.state.markdown;
+        this.oldState.secret = this.state.secret;
+
+        // Disable the buttons.
+        this.render();
     };
 
-    var handleChange = function handleChange() {
-        var $parentForm = $(this).closest(".user-snippet-form");
-        var $previewText = $parentForm.find(".snippet-preview");
-        var $textarea = $parentForm.find("textarea");
+    // Recover the original values from `this.oldState`.
+    Snippet.prototype.undo = function(e) {
+        e && e.preventDefault && e.preventDefault();
+        this.$textarea.val(this.oldState.content);
+        this.$markdownInput.prop("checked", this.oldState.markdown);
+        this.$secretInput.prop("checked", this.oldState.secret);
 
-        var dirty = $textarea.val() !== $textarea.data("orig");
-        $parentForm.toggleClass("dirty", dirty);
-
-        $parentForm.find(".undo-button, .save-button").
-            prop("disabled", !dirty);
-
-        var $preview = $parentForm.find(".snippet-preview-container");
-        if (dirty) {
-            var textVal = $textarea.val();
-            $parentForm.find(".snippet-tag-none")[!textVal ? "show" : "hide"]();
-
-            if ($parentForm.find("input[name='is_markdown']")[0].checked) {
-                textVal = window.marked(textVal);
-            }
-
-            $previewText.html(textVal);
-            $preview.show();
-        } else {
-            $preview.hide();
-        }
+        // HACK: Reset internal state and disable the buttons.
+        this.$inputs.trigger("change");
     };
 
+    // Check if a snippet is "dirty", meaning its initial state no-longer
+    // matches the values of all its inputs.
+    Snippet.prototype.checkIfDirty = function() {
+        return (
+            this.state.content !== this.oldState.content ||
+            this.state.markdown !== this.oldState.markdown ||
+            this.state.secret !== this.oldState.secret
+        );
+    };
+
+    // Run some side-effects on the Snippet's elements. This could be diffed,
+    // but it seems to run well enough as-is.
+    Snippet.prototype.render = function() {
+        var isDirty = this.checkIfDirty();
+
+        this.$el.toggleClass("dirty", isDirty);
+        this.$buttons.prop("disabled", !isDirty);
+        this.$preview[isDirty ? "show" : "hide"]();
+        this.$noneTag[this.state.content ? "hide" : "show"]();
+        this.$previewText.html(this.state.markdown ?
+            window.marked(this.state.content) : this.state.content);
+        this.$secretTag[this.state.secret ? "show" : "hide"]();
+        this.$previewText.toggleClass("snippet-text-markdown",
+            this.state.markdown);
+        this.$previewText.toggleClass("snippet-text", !this.state.markdown);
+    };
+
+    // Catch form submissions, submit and disable buttons.
+    Snippet.prototype.submit = function(e) {
+        e && e.preventDefault && e.preventDefault();
+        $.post(this.$el.attr("action"), this.$el.serialize(),
+            this.save.bind(this));
+    };
+
+    // Create a Snippet for each week shown.
+    var snippets = $(".user-snippet-form").map(function() {
+        return new Snippet($(this));
+    }).get();
+
+    // Apply markdown formatting to all markdown-y snippets.
     window.marked.setOptions({sanitize: true});
     $(".snippet-text-markdown").each(function(i, v) {
         v.innerHTML = window.marked(v.innerHTML);
     });
 
-    // save the state of each textarea to allow undos
-    $(".snippet textarea").each(function(i, v) {
-        saveLocalSnippet(v);
-    });
-
-    $("input[name=private]").on("change", function(e) {
-        var $parentForm = $(this).closest(".user-snippet-form");
-        var $privateTag = $parentForm.find(".snippet-tag-private");
-        $privateTag[this.checked ? "show" : "hide"]();
-        handleChange.call(this);
-    });
-
-    $("input[name=is_markdown]").on("change", function() {
-        var $parentForm = $(this).closest(".user-snippet-form");
-        var $previewText = $parentForm.find(".snippet-preview");
-
-        var isMarkdown = this.checked;
-        $previewText.toggleClass("snippet-text-markdown", isMarkdown);
-        $previewText.toggleClass("snippet-text", !isMarkdown);
-
-        handleChange.call(this);
-    });
-
-    // measure dirtiness on change
-    $(".snippet textarea").on("keyup change", handleChange);
-
-    // catch form submissions, submit and disable buttons
-    $(".user-snippet-form").on("submit", function(e) {
-        e.preventDefault();
-        var vals = $(this).serialize();
-        var $textarea = $(this).find("textarea");
-        var disableUndoOnUpdate = function(d, ts, jqx) {
-            saveLocalSnippet($textarea);
-        };
-        $.post($(this).attr("action"), vals, disableUndoOnUpdate);
-    });
-
-    // undo button behavior
-    $(".snippet .undo-button").on("click", function(e) {
-        e.preventDefault();
-        var $parentForm = $(this).closest(".user-snippet-form");
-        var $textarea = $parentForm.find("textarea");
-        $textarea.val($textarea.data("orig"));
-        $parentForm.removeClass("dirty").
-            find(".undo-button, .save-button").
-            prop("disabled", true);
-        $parentForm.find(".snippet-preview-container").hide();
-    });
-
-    // confirm window closings :)
+    // Confirm window closings :)
     $(window).on("beforeunload", function() {
-        var dirtySnippets = $(".dirty").length;
-        if (dirtySnippets) {
-            var s = dirtySnippets > 1 ? "s." : ".";
-            var msg = "Hey!!! You have " + dirtySnippets +
-                " unsaved snippet" + s;
+        var numDirty = snippets.filter(function(snippet) {
+            return snippet.checkIfDirty();
+        }).length;
+
+        if (numDirty) {
+            var s = numDirty > 1 ? "s." : ".";
+            var msg = "Hey!!! You have " + numDirty + " unsaved snippet" + s;
             return msg;
         }
     });
