961cb666113902574866162aaa4e40c31ed326c6
Craig Silverstein <csilvers@khanacademy.org>
2016-01-12 11:05:47 -0800

    Use ajax to send hide/delete requests.
    Summary:
    Added some basic error-checking, and made it work on firefox.
    
    I don't know the best way to turn an event into an element, but I
    found a way that works on two different browsers at least.  Hopefully
    that's enough...
    
    The error checking is //very// basic, fwiw.
    
    I also made the class names more specific, so we don't match other
    things accidentally.
    
    Test Plan:
    Ran 'make serve' and visited
       http://localhost:8080/admin/manage_users
    as a logged-in-as-admin user (which I had already created).
    
    I then clicked 'hide' for my one user and saw it change to 'hiding...'
    and then 'unhide', I reloaded the page and saw it say 'unhide',
    meaning the setting had stuck.  I then clicked on 'unhide' and saw it
    change back.
    
    I then clicked on 'delete' and saw it change to 'deleting...' and then
    'deleted', and reloaded the page and saw that that user was gone.
    
    Fixes #15
    
    (same)
    
    Reviewers: riley, michelle
    
    Reviewed By: michelle
    
    Subscribers: mroth, benjaminpollack
    
    Differential Revision: https://phabricator.khanacademy.org/D24481

---
+++ static/manage_users.js
@@ -0,0 +1,57 @@
+// Handlers for the manage_users.html template.
+//
+$(function() {
+    // Function to attach to each 'hide' button that toggles the hide state.
+    function toggleHide(event) {
+        var $inputButton = event.toElement || event.target;
+        var email = $($inputButton).attr("data-email");
+        if ($inputButton.value === "Hide") {
+            $inputButton.value = "Hiding...";
+            $inputButton.disabled = true;
+            $.ajax("/admin/manage_users?hide%20" + email)
+                .then(function() {
+                    $inputButton.name = "unhide " + email;
+                    $inputButton.value = "Unhide";
+                    $inputButton.disabled = false;
+                }, function() {
+                    $inputButton.value = "Re-hide (hiding failed!)";
+                    $inputButton.disabled = false;
+                });
+        } else {
+            $inputButton.value = "Unhiding...";
+            $inputButton.disabled = true;
+            $.ajax("/admin/manage_users?unhide%20" + email)
+                .then(function() {
+                    $inputButton.name = "hide " + email;
+                    $inputButton.value = "Hide";
+                    $inputButton.disabled = false;
+                }, function() {
+                    $inputButton.value = "Re-unhide (unhiding failed!)";
+                    $inputButton.disabled = false;
+                });
+        }
+    }
+
+    function doDelete(event) {
+        var $inputButton = event.toElement || event.target;
+        var email = $($inputButton).attr("data-email");
+        $inputButton.value = "Deleting...";
+        $inputButton.disabled = true;
+        $.ajax("/admin/manage_users?delete%20" + email)
+            .then(function() {
+                $inputButton.value = "Deleted";
+                // TODO(csilvers): change the whole row to indicate deleted
+            }, function() {
+                $inputButton.value = "Re-delete (deleting failed!)";
+                $inputButton.disabled = false;
+            });
+    }
+
+    $(".hide-account-button").map(function() {
+        $(this).on("click", toggleHide.bind(this));
+    });
+
+    $(".delete-account-button").map(function() {
+        $(this).on("click", doDelete.bind(this));
+    });
+});
