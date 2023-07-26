b9670eadc112e35cc7bd9f0dd9537baae2557003
Benjamin Staffin <bstaffin@singlestore.com>
2023-07-26 12:54:33 -0400

    Another pep8 nit

--- conftest.py
+++ conftest.py
@@ -25,7 +25,8 @@ def ndb_context(init_ndb_env_vars):
 
 @pytest.fixture(autouse=True)
 def init_ndb_env_vars(monkeypatch: MonkeyPatch) -> None:
-    """
+    """Set environment variables for the test ndb client.
+
     Initializing an ndb Client in a test env requires some environment variables
     to be set. For now, these are just garbage values intended to give the
     library _something_ (we don't expect them to actually work yet)
